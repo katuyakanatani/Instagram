a72c63e27fa35f57b5956ff8a9d1be68a3759ac0
katuya.kanatani <bouka.tenken@gmail.com>
2020-01-29 22:46:39 +0900

    完成前

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Instagram
+//
+//  Created by USER on 2020/01/29.
+//  Copyright © 2020 katuya.kanatani. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CLImageEditor
 
-class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLImageEditorDelegate {
     @IBAction func handleLibraryButton(_ sender: Any) {
         // ライブラリ（カメラロール）を指定してピッカーを開く
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
@@ -39,9 +40,20 @@ class ImageSelectViewController: UIViewController, UIImagePickerControllerDelega
 
               // あとでCLImageEditorライブラリで加工する
               print("DEBUG_PRINT: image = \(image)")
+            //CLImageEditorにimageを渡してl、加工画面を起動する
+            let editor = CLImageEditor(image: image)!
+            editor.delegate = self
+            picker.present(editor, animated: true, completion: nil)
 
           }
       }
+    // CLImageEditorで加工が終わったときに呼ばれるメソッド
+    func imageEditor(_ editor: CLImageEditor!, didFinishEditingWith image: UIImage!) {
+        // 投稿画面を開く
+        let postViewController = self.storyboard?.instantiateViewController(withIdentifier: "Post") as! PostViewController
+        postViewController.image = image!
+        editor.present(postViewController, animated: true, completion: nil)
+    }
 
       func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
           // ImageSelectViewController画面を閉じてタブ画面に戻る
